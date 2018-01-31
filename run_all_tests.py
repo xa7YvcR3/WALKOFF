3a5d95a0a494029f28460bd96e51eba34db991fd
adpham95 <32873177+adpham95@users.noreply.github.com>
2018-01-31 11:07:36 -0500

    added optional arguments to run only certain test suites

--- run_all_tests.py
+++ run_all_tests.py
@@ -7,30 +7,55 @@ import sys
 from tests import suites as test_suites
 import logging
 import walkoff.server.context
+import argparse
 
 
-def run_tests():
+def run_tests(arg):
     logging.disable(logging.CRITICAL)
 
     ret = True
-    print('Testing Integration:')
-    ret &= unittest.TextTestRunner(verbosity=1).run(test_suites.integration_suite).wasSuccessful()
-    print('Testing Workflows:')
-    ret &= unittest.TextTestRunner(verbosity=1).run(test_suites.workflow_suite).wasSuccessful()
-    print('\nTesting Execution:')
-    ret &= unittest.TextTestRunner(verbosity=1).run(test_suites.execution_suite).wasSuccessful()
-    print('\nTesting Cases:')
-    ret &= unittest.TextTestRunner(verbosity=1).run(test_suites.case_suite).wasSuccessful()
-    print('\nTesting Server:')
-    ret &= unittest.TextTestRunner(verbosity=1).run(test_suites.server_suite).wasSuccessful()
-    print('\nTesting Interface:')
-    ret &= unittest.TextTestRunner(verbosity=1).run(test_suites.interface_suite).wasSuccessful()
+    if arg.all or arg.integration:
+        print('Testing Integration:')
+        ret &= unittest.TextTestRunner(verbosity=1).run(test_suites.integration_suite).wasSuccessful()
+
+    if arg.all or arg.workflows:
+        print('Testing Workflows:')
+        ret &= unittest.TextTestRunner(verbosity=1).run(test_suites.workflow_suite).wasSuccessful()
+
+    if arg.all or arg.execution:
+        print('\nTesting Execution:')
+        ret &= unittest.TextTestRunner(verbosity=1).run(test_suites.execution_suite).wasSuccessful()
+
+    if arg.all or arg.cases:
+        print('\nTesting Cases:')
+        ret &= unittest.TextTestRunner(verbosity=1).run(test_suites.case_suite).wasSuccessful()
+
+    if arg.all or arg.server:
+        print('\nTesting Server:')
+        ret &= unittest.TextTestRunner(verbosity=1).run(test_suites.server_suite).wasSuccessful()
+
+    if arg.all or arg.interface:
+        print('\nTesting Interface:')
+        ret &= unittest.TextTestRunner(verbosity=1).run(test_suites.interface_suite).wasSuccessful()
+
     return ret
 
 
 if __name__ == '__main__':
     try:
-        successful = run_tests()
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-a", "--all", action="store_true")
+        parser.add_argument("-ig", "--integration", action="store_true")
+        parser.add_argument("-w", "--workflows", action="store_true")
+        parser.add_argument("-e", "--execution", action="store_true")
+        parser.add_argument("-c", "--cases", action="store_true")
+        parser.add_argument("-s", "--server", action="store_true")
+        parser.add_argument("-if", "--interface", action="store_true")
+        args = parser.parse_args()
+        if len(sys.argv) == 1:
+            args.all = True
+
+        successful = run_tests(args)
     except KeyboardInterrupt:
         print('\nInterrupted! Ending full test')
         successful = False
